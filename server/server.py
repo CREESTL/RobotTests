46a2677f00727cbc3286878425012798bec7872f
CREESTL <kroymw3@yandex.ru>
2021-05-13 00:41:21 +0300

    Tested Server

--- server/server.py
+++ server/server.py
@@ -2,9 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+# TODO add all routes from the doc here
+# TODO Take json replies with according names from json/replies_examples folder
+
+
+@app.route('/api//test')
 def index():
-    return 'Hello world'
+    return {"main_text": "This is a test page"}
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
